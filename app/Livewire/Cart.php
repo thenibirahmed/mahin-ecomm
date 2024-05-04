9d0f7fb935ec2fa94519eea785808ccfc3019d9a
Nibir Ahmed <nibirahmed9611@gmail.com>
2024-05-04 23:42:53 +0600

    working

--- app/Livewire/Cart.php
+++ app/Livewire/Cart.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class Cart extends Component
 {
+    public function removeFromCart($index)
+    {
+        $carts = session()->get('cart', []);
+        unset($carts[$index]);
+        session()->put('cart', $carts);
+    }
+
     public function render()
     {
         return view('livewire.cart',[
--- resources/views/livewire/cart.blade.php
+++ resources/views/livewire/cart.blade.php
@@ -1,28 +1,38 @@
-<section class="py-5">
-    <div class="container px-4 px-lg-5 mt-5">
-        @if (session()->has('message'))
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
-                {!! session('message') !!}
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+
+<div>
+    <header class="bg-dark py-5">
+        <div class="container px-4 px-lg-5 my-5">
+            <div class="text-center text-white">
+                <h1 class="display-4 fw-bolder">Cart</h1>
+                <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
             </div>
-        @endif
+        </div>
+    </header>
+    <section class="py-5">
+        <div class="container px-4 px-lg-5 mt-5">
+            @if (session()->has('message'))
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    {!! session('message') !!}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+            <table class="table">
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Action</th>
+                </tr>
+                @foreach ($carts as $index => $cart)
+                <tr>
+                    <td>{{ $cart['name'] }}</td>
+                    <td>{{ $cart['price'] }}</td>
+                    <td>{{ $cart['quantity'] }}</td>
+                    <td><button wire:click="removeFromCart({{ $index }})" class="btn btn-danger">X</button></td>
+                </tr>
+                @endforeach
+            </table>
 
-        <table class="table">
-            <tr>
-                <th>Name</th>
-                <th>Price</th>
-                <th>Quantity</th>
-                <th>Action</th>
-            </tr>
-            @foreach ($carts as $cart)
-            <tr>
-                <td>{{ $cart['name'] }}</td>
-                <td>{{ $cart['price'] }}</td>
-                <td>{{ $cart['quantity'] }}</td>
-                <td><button class="btn btn-danger">X</button></td>
-            </tr>
-            @endforeach
-        </table>
-
-    </div>
-</section>
+        </div>
+    </section>
+</div>
